c82161ce3112bbef440af6c2053920dc995ca1ed
Emma Plante <zerox029@gmail.com>
2024-01-31 15:35:20 -0500

    Added testing support

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,31 +7,19 @@
 use toast::println;
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     toast::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
