485caebf2e4be71f45a935fcc8a7943d087b2f9b
Emma Plante <zerox029@gmail.com>
2024-03-10 14:46:26 -0400

    Added test runner

--- kernel/debug/deps/autocfg-20828532232d1b47.d
+++
@@ -1,9 +0,0 @@
-/home/emma/Desktop/toast/kernel/./debug/deps/libautocfg-20828532232d1b47.rmeta: /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/emma/Desktop/toast/kernel/./debug/deps/libautocfg-20828532232d1b47.rlib: /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/emma/Desktop/toast/kernel/./debug/deps/autocfg-20828532232d1b47.d: /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs:
-/home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs:
-/home/emma/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs:
